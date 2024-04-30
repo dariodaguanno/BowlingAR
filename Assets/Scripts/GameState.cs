057c826188ec986e0b5825d1bd643a8cbdab4ccf
dariodaguanno <tul11082@temple.edu>
2024-04-30 19:15:18 -0400

    player controller added, game state asset

---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "GameState", menuName = "ScriptableObjects/CreateGameStateAsset")]
+public class GameState : ScriptableObject { 
+    
+    public enum GameStateEnum {
+
+        TitleScreen,
+        PlacingPinDeckAndLane,
+        SetupBalls,
+        ReadyToThrow,
+        BallInPlay,
+        BallPlayEnd,
+        StrikeAchieved,
+        TurnEnd,
+        ResettingDeck,
+        GameEnded
+
+    }
+
+    [SerializeField] private GameStateEnum currentGameState;
+
+    public GameStateEnum CurrentGameState {
+        get => currentGameState;
+        set => currentGameState = value;
+    }
+}
